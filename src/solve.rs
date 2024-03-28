4f1c2ea91e185f6611ce380800b6557dac8926f7
Luca <james.lol2000@gmail.com>
2024-03-28 20:55:06 +0100

    Add empty solver as example

--- src/solve.rs
+++ src/solve.rs
@@ -2,6 +2,9 @@ use std::fmt::Display;
 
 use crate::{board::Board, piece::Piece};
 
+pub mod dumb_solver;
+mod empty_solver;
+
 pub type SolveResult<T> = Result<Board, T>;
 
 pub trait Solvable {
@@ -15,8 +18,6 @@ pub trait Solvable {
         pieces: &[Vec<Piece>],
     ) -> SolveResult<Self::Failure>;
 }
-
-pub mod dumb_solver;
 
 pub fn solve<S: Solvable>(
     solver: S,
